f953335e1334637d0ace6a8415d49dfa9e97fa1c
Abass Koyang <abasskoyang12345@gmail.com>
2023-10-22 06:58:18 +0930

    initial commit

---
+++ components/SearchBar.tsx
@@ -0,0 +1,24 @@
+"use client";
+import React from 'react'
+import Image from "next/image";
+import { SearchManufacturer } from ".";
+import {useState} from 'react';
+
+const SearchBar = () => {
+
+    const [manufacturer, setManufacturer] = useState('')
+    const handleSearch = () => {}
+
+  return (
+    <form action="" className="searchbar" onSubmit={handleSearch}>
+        <div className="searchbar__item">
+            <SearchManufacturer
+            manufacturer={manufacturer}
+            setManufacturer={setManufacturer} 
+            />
+        </div>
+    </form>
+  )
+}
+
+export default SearchBar
---
+++ components/index.ts
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "@/components/CarCard";
+import CarDetails from "@/components/CarDetails"
+export {
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer,
+    CarCard,
+    CarDetails,
+}
